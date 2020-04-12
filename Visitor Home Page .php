05a6b5ec8abc11d01770e6e278fddc0013d58028
Hadeel Hamad <xhadeel246@gmail.com>
2020-04-12 03:41:49 +0300

    Add files via upload

---
+++ Visitor Home Page .php
@@ -0,0 +1,543 @@
+ <!-- V.5 -->
+
+
+ <?php 
+session_start();
+//if(!isset($_SESSION['userName'])){
+ // header("location: index.php");
+  //exit();}
+
+include "DBconnection.php";
+
+ ?>
+
+<!DOCTYPE html>
+<html>    
+<head>
+  <!-- Site made with Mobirise Website Builder v4.12.0, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.12.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="indexassets/images/fulllogo.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+
+  <link rel="stylesheet" href="indexassets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="indexassets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="indexassets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="indexassets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="indexassets/tether/tether.min.css">
+  <link rel="stylesheet" href="indexassets/dropdown/css/style.css">
+  <link rel="stylesheet" href="indexassets/theme/css/style.css">
+  <link rel="preload" as="style" href="indexassets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="indexassets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  <style>
+      .nav-item{margin-right: 10em;
+      }
+      .contactUs {margin-left: 30em}
+      #gallery1-7 {background-color:#efefef;
+      align-content: center;
+      padding-top: -5em;
+    }
+      #content10-8 {background-color:#e3cd52}
+      #Galary {color: black;}
+      #aboutus {text-align: center;
+      
+      }
+       #aboutus strong{font-size: 30pt;}
+      
+      .contactUs {margin-left: 25em}
+      .contactUs strong {margin-left: 3em}
+      .contactUs p a {padding-right: 3em;}
+      #search-2 {
+          position: fixed;
+          overflow: scroll;
+          background-color: white;
+          border-radius: 10px;
+          display: none;
+          z-index: 10;
+          top :74px;
+          right: 360px;
+          border: 4px;
+          border-color: black;
+          max-height: 350px;
+      }
+      .Artworks{
+          margin-left:50px;
+          height: 30px; 
+          width: 200px;
+          border: solid; 
+          border-left-width: 5px;
+          border-right-width: 0px; 
+          border-bottom-width: 0px; 
+          border-top-width: 0px; 
+          border-left-color: #feeb7f; 
+          margin-top: 5px;
+          margin-left: 25px;
+          font-size: 20px;
+          display: none;
+          
+
+      }
+      .Artworks:hover{
+        border-left-color: gray;   
+      }
+      #filterInput{
+          background-color : #f6f6f6 ;
+          margin-left:0px;  
+          margin-top:17px; 
+          height: 50px; 
+          width: 380px; 
+          border: solid;
+          border-color: #feeb7f; 
+          border-radius:50px;
+          font-size: 20px
+      }
+      
+      .titles{
+          color: gray;
+      }
+      
+      #header2-0{
+          animation: myfirst 50s ease-in .1s infinite ;
+
+      }
+      
+      
+      @keyframes myfirst{
+          
+          0%{
+             background-image: url(indexassets/images/art0.jpg); 
+          }
+          10%{
+             background-image: url(indexassets/images/art1.jpg); 
+          }
+          20%{
+             background-image: url(indexassets/images/art2.jpg); 
+          }
+          30%{
+             background-image: url(indexassets/images/art3.jpg); 
+          }
+          40%{
+             background-image: url(indexassets/images/art4.jpg); 
+          }
+          50%{
+             background-image: url(indexassets/images/art5.jpg); 
+          }
+          60%{
+             background-image: url(indexassets/images/art6.jpg); 
+          }
+          70%{
+             background-image: url(indexassets/images/art7.jpg); 
+          }
+          80%{
+             background-image: url(indexassets/images/art8.jpg); 
+          }
+          90%{
+             background-image: url(indexassets/images/art9.jpg); 
+          }
+          100%{
+             background-image: url(indexassets/images/art5.jpg); 
+          }
+          
+          
+      }
+      
+      
+    </style>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    
+     <!--paste your head code-->
+    
+  <title>Visitor Home Page</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/gallery/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+    <!----------------------->
+  
+</head>
+<body>
+  <section class="menu cid-rQpmxxKyZ3" once="menu" id="menu2-6">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="#">
+                        <img src="indexassets/images/fulllogo.png" alt="Mobirise" title="" style="height: 4.6rem;">
+                    </a>
+                </span>
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+                <li class="nav-item" >
+                    <a class="nav-link link text-white display-5" href="#">
+                        Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-5" href="#aboutus">
+                        About us</a>
+                </li><li class="nav-item" >
+                    <a class="nav-link link text-white display-5" href="#conus">
+                        Contact us</a>
+                </li><li class="nav-item" >
+                    <a class="nav-link link text-white display-5" href="Visitor%20account%20Page%20.php">
+                        My account</a>
+                </li>
+                <div class="search">
+                 <li class="nav-item" >
+                    <a id = "search1" class="nav-link link text-white display-5" ><img src="indexassets/search.png" width="25" height="28"></a>
+                </li>
+                    </div>
+              <div class="signout">
+                <li class="nav-item" >
+                    <a id="signout3" class="nav-link link text-white display-5" href="index.php"><img src="indexassets/singout.png" width="25" height="28"></a>
+                </li>
+                  </div>
+            </ul>
+            
+        </div>
+    </nav>
+</section>
+    
+    
+    <script>
+        
+        $(document).ready(function(){
+  $(".signout").hover(function(){
+    //hover
+      document.getElementById("signout3").innerHTML="<img src='indexassets/singout2.png' width='25' height='28'>";
+  },
+  function(){
+    //out
+      document.getElementById("signout3").innerHTML="<img src='indexassets/singout.png' width='25' height='28'>";
+  }); 
+});
+     
+        
+        $(document).ready(function(){
+  $(".search").hover(function(){
+    //hover
+      document.getElementById("search1").innerHTML="<img src='indexassets/search2.png' width='25' height='28'>";
+  },
+  function(){
+    //out
+      document.getElementById("search1").innerHTML="<img src='indexassets/search.png' width='25' height='28'>";
+  }); 
+});
+        
+        
+        
+    </script>
+  
+    
+    
+
+<section class="mbr-fullscreen mbr-parallax-background" id="header2-0">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-3">Welcome</h1>
+                
+                <p class="mbr-text pb-3 mbr-fonts-style display-5">Paint &amp; Pixel&#44; the ideal place for digital art enthusiasts</p>
+                
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true" id="circul" style = "position:absolute; left:565px; top:600px;">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont" style = "position:absolute; left: -15px"></i>
+        </a>
+        </div></div>
+</section>
+
+     <div id = "search-2"  placeholder="Search names...">
+    
+         <form id="searchTitle">
+         <input id="filterInput" type="text">
+             
+
+         
+         </form>
+         
+            
+         
+    </div>
+    
+     <script>
+       
+         $(document).ready(function(){
+  $("#search1").click(function(){
+    $("#search-2").toggle();
+  });
+}); 
+         
+         <?php
+              $query = "SELECT title FROM Artwork";
+             $result = mysqli_query($conn,$query);
+         
+         
+ while($Arr = mysqli_fetch_array($result)){
+             $art = $Arr["title"];
+             ?>
+            var art = "<?php echo $art ?>";
+            var node = document.createElement("div");
+            node.setAttribute("class","Artworks");
+            document.getElementById('searchTitle').appendChild(node);
+     
+            node.innerHTML = "<a href='View%20Artwork%20(Artist).php 'class='titles' >&nbsp;&nbsp;"+art+"<a>" ;
+         
+   <?php } ?>
+ 
+         
+         
+         // Get input element
+    let filterInput = document.getElementById('filterInput');
+    // Add event listener
+    filterInput.addEventListener('keyup', filterNames);
+
+    function filterNames(){
+
+      // Get value of input
+      let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
+      // Get names ul
+      let allTitles = document.getElementById('searchTitle');
+      // Get lis from ul
+      let arts = allTitles.querySelectorAll('div.Artworks');
+
+      // Loop through collection-item lis
+      for(let i = 0;i < arts.length;i++){
+        let a = arts[i].getElementsByClassName('titles')[0];
+        // If matched
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+          arts[i].style.display = 'block';
+        } else {
+          arts[i].style.display = 'none';
+        }
+      }
+
+    }
+         
+         
+ 
+    </script>
+    
+    
+    
+    
+    <!--paste your body code-->
+    
+       <section class="mbr-section article content10 cid-rReDTdKj6V" id="content10-8">
+    
+
+    <div class="container">
+        <div class="inner-container" style="width: 68%;">
+            <hr class="line" style="width: 25%;">
+            <div class="section-text align-center mbr-white mbr-fonts-style display-1" id = "Galary">Galary</div>
+            <hr class="line" style="width: 25%;">
+        </div>
+    </div>
+</section>
+
+
+
+<section class="mbr-gallery mbr-slider-carousel cid-rReCpFQ8Rc" id="gallery1-7">
+
+<a href="favouriteList.php"class="col-md-5 input-group-btn align-left"><button id="updateButton" name ="submit" class="btn btn-form btn-primary display-4"><span class="mobi-mbri mobi-mbri-refresh mbr-iconfont mbr-iconfont-btn" style="color: rgb(69, 80, 91);"></span>My favorite list</button></a>
+                    </div>
+<?php
+
+$query = "SELECT * FROM artwork; ";
+    
+ $result = mysqli_query($conn,$query) ;  
+    if(!$result) 
+     print 'error';
+     
+
+?>
+    
+<div>
+        <div><!-- Filter --><!-- Gallery -->
+
+
+<div class="mbr-gallery-row">
+  <div class="mbr-gallery-layout-default">
+    <div>
+      <div>
+
+<?php 
+$count=0;
+ while($row=mysqli_fetch_assoc($result))  {  
+$href='View%20Artwork%20(visitor).php?title=' . $row['title'] ;
+$src=$row['path'];
+$title=$row['title'];
+$pathsArray[]=$src;
+
+  ?>
+
+<a href ="<?php  echo $href;  ?>"  >
+              <div class="mbr-gallery-item mbr-gallery-item--p1" >
+                <div href="<?php  echo $href;  ?>" data-slide-to="<?php  echo $count;  ?>" data-toggle="modal">
+                  <img src="<?php  echo $src;  ?>" alt="" title="">
+                  <span class="icon-focus"></span>
+                  <span class="mbr-gallery-title mbr-fonts-style display-7"> <?php  echo $title;  ?></span>
+                </div></div>
+              </a>
+
+<?php  $count++; }
+?>
+  
+            </div></div>
+            <div class="clearfix"></div>
+          </div></div> 
+
+          
+     <!-- Lightbox --><div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery1-7">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-body">
+                    <ol class="carousel-indicators">
+ 
+<script type="text/javascript">
+  var count= <?php echo $count; ?>;
+  for (var i = 0; i <count; i++) {
+    document.write('<li data-app-prevent-settings="" data-target="#lb-gallery1-7" data-slide-to="'+count+'"></li>');
+  }
+</script>
+      
+</ol>
+
+                    <div class="carousel-inner">
+
+<script type="text/javascript">
+
+  var paths[]= <?php echo $pathsArray; ?>;
+  for (var i = 0; i <pathsArray.lenght; i++) {
+    document.write('<div class="carousel-item active"><img src="'+paths[i]+'" ></div>');
+  }
+</script>
+                      </div>
+
+
+
+                    </div></div></div></div></div>
+    </div>
+
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/gallery/player.min.js"></script>
+  <script src="assets/gallery/script.js"></script>
+  <script src="assets/slidervideo/script.js"></script>
+  
+  
+    <!----------------------->
+     
+
+<section class="mbr-section article content9 cid-rQplkhn7NZ" id="content9-4">
+
+    <div class="container">
+        <div class="inner-container" style="width: 100%;">
+            <hr class="line" style="width: 100%;">
+            <div class="section-text align-center mbr-fonts-style display-5" id ="aboutus"><strong><em>About us</em></strong><br>First, we’d like to thank you for visiting our website.
+<div>Digital Art is a brand-new online platform for artists to upload their artwork for the world to see. Visitors who have registered can view our gallery and comment on artworks, creating a supportive environment for both up-and-coming digital artists as well as professional illustrators.
+</div><div>Please support our website by voting for it in our competition!
+</div><div>This website is brought to you by the following future software engineers:
+</div><div>•	Sarah Algwaiz
+</div><div>•	Shahad Alkaltham
+</div><div>•	Hadeel Alhajri
+</div><div>•	Ghada Alshridah
+</div><div><p></p></div>
+            </div></div> </div>
+</section>
+
+
+<section class="mbr-section article content9 cid-rQpllaL4cw" id="content9-5">
+    
+     
+
+    <div class="container" id="conus">
+        <div class="inner-container" style="width: 100%;">
+            <hr class="line" style="width: 100%;">
+            <div class ="contactUs">
+            <div class="section-text align-center mbr-fonts-style display-5"><strong>Contact us</strong>
+                
+            </div><p>
+            <a href="https://twitter.com"><img src = "indexassets/twitter.png" width="32" height="26"></a> <a href="https://www.facebook.com"><img src = "indexassets/facebook.png" width="28" height="28"></a>
+            <a href="https://www.instagram.com"><img src="indexassets/instagram.png" width="30" height="30" ></a> 
+            <a href="mailto:xhadeel@hotmail"><img src="indexassets/email.png" width="32" height="21"></a>
+            </p>
+                </div>
+            <hr class="line" style="width: 100%;">
+        
+        </div>
+    </div>
+</section>
+
+<section once="footers" class="cid-rQpl2pRx1d" id="footer6-2">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    © Copyright 2020 Paint&amp;Pixel Team - All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="indexassets/web/assets/jquery/jquery.min.js"></script>
+  <script src="indexassets/popper/popper.min.js"></script>
+  <script src="indexassets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="indexassets/tether/tether.min.js"></script>
+  <script src="indexassets/smoothscroll/smooth-scroll.js"></script>
+  <script src="indexassets/dropdown/js/nav-dropdown.js"></script>
+  <script src="indexassets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="indexassets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="indexassets/parallax/jarallax.min.js"></script>
+  <script src="indexassets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
